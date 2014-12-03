dc62f0b91be01731fce648822c39afa63b34d58a
mackentan <jundongtan@gmail.com>
2014-12-03 18:47:37 +0800

    modify pub

--- doc/build.sh
+++ doc/build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+SPATH=$(cd "$(dirname "$0")"; pwd)
 export GOROOT="/macken/go"
+SRCPATH=$SPATH/../src
+export GOPATH=$SRCPATH
 
 
-$GOROOT/bin/go build GoProxyHunt.go
+$GOROOT/bin/go build $SRCPATH/GoProxyHunt.go
 
